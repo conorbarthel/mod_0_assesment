ba46652e87ac3d114438d4ed8a8e3959b15e4212
Conor Barthel <conor.barthel@gmail.com>
2021-11-12 08:20:50 -0800

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+animal = "Rhyno"
+is_cuddly = false
+animal = "Dog"
+num_8 = 21
+num_3 = 63
+quotient = num_3 / num_8
+favorite_films = ["Princess Mononoke", "Empire Strikes Back", "City of God"]
+p favorite_films.shift()
+p favorite_films.length()
