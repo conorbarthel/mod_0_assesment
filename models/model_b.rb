4d4661fafcaeb6b915b0d1b5409647cf9ecab9ff
Conor Barthel <conor.barthel@gmail.com>
2021-11-12 08:41:48 -0800

    Add home hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+home = {
+  address: 4579,
+  street: "Elm",
+  city: "Denver"
+  zip: 80234,
+  ocupied: true,
+  has_garage: false,
+  rooms: ["bedroom", "bathroom", "office", "livingroom", "basement", "diningroom", "kitchen"],
+  occupants: ["Caitlin", "Conor"]
+}
