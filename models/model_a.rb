09ed2213048ece54107f22559ffab571b962bc30
Conor Barthel <conor.barthel@gmail.com>
2021-11-12 08:32:31 -0800

    Add laptop hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+personal_laptop = {
+  welcome_message: "Hello, booting up",
+  password_request: "Enter password",
+  logged_in: true,
+  number_of_files: 543,
+  operating_system: 6.03,
+  settings_configured: true,
+  applications: ["message", "e-mail", "text editor"],
+  desktop_files: ["profile pic", "resume", "cover letter"]
+}
